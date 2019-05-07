a3319cd61e3d0a9b111be798ecad44a4386efe66
trongnv <trongnv@ows.vn>
2019-05-07 15:08:39 +0700

    add file run

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # semantic_analysis
+How to run file : 
+1. cd [name_folder]
+2. sh run.sh
---
+++ incompleted/run.sh
@@ -0,0 +1,2 @@
+gcc -o main main.c parser.c scanner.c reader.c charcode.c token.c error.c symtab.c debug.c
+./main
